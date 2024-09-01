6892e0350e57dc693f137ed9f205a3653ff4d45c
abbasshaikh <abbas.shaikh.ext@intive.com>
2024-09-01 15:53:48 +0530

    Removed commented code

--- users/serializers.py
+++ users/serializers.py
@@ -23,9 +23,6 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 
-# class LoginSerializer(serializers.Serializer):
-#     email = serializers.EmailField()
-#     password = serializers.CharField(write_only=True)
 class UserDetailModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserDetails
--- users/views.py
+++ users/views.py
@@ -2,7 +2,6 @@ from rest_framework import status, generics, filters
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.pagination import PageNumberPagination
 
 from common.custom_pagination import ListingPaginator
 from .models import UserDetails
@@ -19,19 +18,6 @@ class RegisterAPIView(APIView):
         return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
 
-# class LoginView(APIView):
-#     def post(self, request, *args, **kwargs):
-#         serializer = LoginSerializer(data=request.data)
-#         if serializer.is_valid():
-#             email = serializer.validated_data['email'].lower()
-#             password = serializer.validated_data['password']
-#             user = authenticate(email=email, password=password)
-#             if user:
-#                 return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
-#             return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 class LoginAPIView(TokenObtainPairView):
     serializer_class = UserTokenObtainPairSerializer
 
@@ -41,18 +27,6 @@ class LoginAPIView(TokenObtainPairView):
         result = super(LoginAPIView, self).post(request, *args, **kwargs)
         return result
 
-
-# class SearchUserAPIView(APIView):
-#     permission_classes = [IsAuthenticated]
-#     pagination_class = PageNumberPagination
-#
-#     def get(self, request, *args, **kwargs):
-#         keyword = request.query_params.get('search', '').lower()
-#         users = UserDetails.objects.filter(email__iexact=keyword) | UserDetails.objects.filter(name__icontains=keyword)
-#         paginator = self.pagination_class()
-#         result_page = paginator.paginate_queryset(users, request)
-#         serializer = UserDetailModelSerializer(result_page, many=True)
-#         return paginator.get_paginated_response(serializer.data)
 
 class SearchUserAPIView(generics.ListAPIView):
     queryset = UserDetails.objects.all()
